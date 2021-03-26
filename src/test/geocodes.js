c5a2b8922a2398850f4d7068921f92c252181616
David Valentine <david.valentine@gmail.com>
2021-03-25 21:00:31 -0700

    Cleanup

--- src/test/geocodes.js
+++ src/test/geocodes.js
@@ -34,35 +34,6 @@ describe("Gecodes", function () {
         )
         // })
         ;
-        describe("dupe test for geodex", function () {
-            let key = "geodex"
-            let template = fullTextTests.fullTextTests.templates.get(key)
 
-            // templates.forEach(function (template,key) {
-            let tests = fullTextTests.fullTextTests.tests
-            let server = fullTextTests.fullTextTests.server
-            let name = fullTextTests.fullTextTests.name
-
-            fullTextTests.fullTextTests.tests.forEach(
-                function (t) {
-                    it(`${key} duplicate full text for ${t.name} `, async function () {
-
-                        const response = await results.results(template, t.params, server)
-                        var b = await response.data.results.bindings
-                        // dupe
-                        console.log(`results.bindings.length ${b.length}`)
-                        const gb = _.groupBy(b, s =>  s.s.value )
-                        const dupes = _.pickBy(gb,x => x.length > 1)
-                        Object.keys(dupes).should.have.length(0)
-                        console.log(dupes)
-
-
-                    }).timeout(10000)
-                }
-            )
-            // })
-            ;
-
-        });
     });
 });
